7485bfe6b30d9ffe2f6a862cd5542fa4ebbd71cc
sarapapi <57095209+sarapapi@users.noreply.github.com>
2026-08-08 16:46:39 +0000

    Prepare branch for Github Page

--- analysis/noisy_fleurs_ambient/combine_csv.sh
+++
@@ -1,6 +0,0 @@
-BASE_PATH=""
-
-python3 $BASE_PATH/hearing2translate/analysis/noisy_fleurs_babble/combine_csv.py \
-    -i $BASE_PATH/hearing2translate/analysis/noisy_fleurs_ambient/noisy_fleurs_*.csv \
-    -oc $BASE_PATH/out_tables/noisy_ambient_combined.csv\
-    -ot $BASE_PATH/out_tables/noisy_ambient_combined.tex \
--- analysis/noisy_fleurs_babble/combine_csv.sh
+++
@@ -1,6 +0,0 @@
-BASE_PATH=""
-
-python3 $BASE_PATH/hearing2translate/analysis/noisy_fleurs_babble/combine_csv.py \
-    -i $BASE_PATH/hearing2translate/analysis/noisy_fleurs_babble/noisy_fleurs_*.csv \
-    -oc $BASE_PATH/out_tables/noisy_babble_combined.csv\
-    -ot $BASE_PATH/out_tables/noisy_babble_combined.tex \
